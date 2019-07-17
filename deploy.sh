f8dcfda61302ce5d5024c54e93e9a001c6ace1ba
MZabolotny <mzabolotnyi@gmail.com>
2019-07-17 14:00:05 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,9 @@ rm -rf var/logs/dev.log var/logs/prod.log var/logs/test.log
 echo -e "Clearing var files was successfully done."
 
 # Composer install
-/usr/bin/php72 /usr/bin/composer install > /dev/null 2>&1
+php /usr/bin/composer install
 echo -e "Composer install done."
 
 # Update DB
-/usr/bin/php72 bin/console doctrine:migration:migrate --no-interaction
+php bin/console doctrine:migration:migrate --no-interaction
 echo -e "Database was updated successfully"
-
-# Set needed permissions for app folders
-chmod 0777 -R var/cache/ var/logs/
